9bba9756dced3bf276d412da86c6ef5314d27abc
hyoguoo <hyoguoo@gmail.com>
2025-08-14 17:08:49 +0900

    init: init

---
+++ src/main/java/hyoguoo/ddl_data_generator/DdlDataGeneratorApplication.java
@@ -0,0 +1,12 @@
+package hyoguoo.ddl_data_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdlDataGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdlDataGeneratorApplication.class, args);
+    }
+}
